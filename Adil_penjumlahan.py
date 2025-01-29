ba51279a3c202c0bd43efdad8f9eba7422d861ff
Adil <alrizkyadil@gmail.com>
2025-01-29 18:38:55 +0700

    Add files via upload

---
+++ Adil_penjumlahan.py
@@ -0,0 +1,4 @@
+angka1 = int(input('Masukakan angka 1: '))
+angka2 = int(input('Masukakan angka 2: '))
+penjumlahan = angka1 + angka2
+print(angka1, "+", angka2, "=", penjumlahan)
---
+++ Adil_rerata.py
@@ -0,0 +1,14 @@
+Nilai01 = int(input("Masukan nilai 1: ")) 
+Nilai02 = int(input("Masukan nilai 2: "))
+Nilai03 = int(input("Masukan nilai 3: "))
+Nilai04 = int(input("Masukan nilai 4: "))
+Nilai05 = int(input("Masukan nilai 5: "))
+Nilai06 = int(input("Masukan nilai 6: "))
+Nilai07 = int(input("Masukan nilai 7: "))
+Nilai08 = int(input("Masukan nilai 8: "))
+Nilai09 = int(input("Masukan nilai 9: "))
+Nilai10 = int(input("Masukan nilai 10: "))
+
+semua_nilai = [Nilai01, Nilai02, Nilai03, Nilai04, Nilai05, Nilai06, Nilai07, Nilai08, Nilai09, Nilai10]
+rata_rata = sum(semua_nilai) / len(semua_nilai)
+print("Rata-rata nilai adalah :", rata_rata)
